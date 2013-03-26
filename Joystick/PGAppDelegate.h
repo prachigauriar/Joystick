c52d38591427575a8dad161fabdc8f3429836226
Prachi Gauriar <prachi@quantumlenscap.com>
2013-03-26 15:00:17 -0400

    Initial Commit

---
+++ Joystick/PGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PGAppDelegate.h
+//  Joystick
+//
+//  Created by Prachi Gauriar on 3/26/2013.
+//  Copyright (c) 2013 Prachi Gauriar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PGAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Joystick/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Joystick
+//
+//  Created by Prachi Gauriar on 3/26/2013.
+//  Copyright (c) 2013 Prachi Gauriar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
